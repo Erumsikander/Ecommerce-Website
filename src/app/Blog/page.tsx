017be698f0cf1113248c4444b2b3fe6bd1299a34
Erumsikander <mrsikanderghouri@gmail.com>
2024-12-17 10:30:28 +0500

    update

--- src/app/Blog/page.tsx
+++ src/app/Blog/page.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
 import { IoIosArrowForward } from "react-icons/io";
-import { Poppins } from 'next/font/google';
 
 const page = () => {
   return (
